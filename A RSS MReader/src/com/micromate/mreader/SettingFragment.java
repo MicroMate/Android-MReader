fc95ec1d629393d90e1345436c4b8d85c953f0ed
Michal Matczak <matczak.wln@gmail.com>
2014-05-29 18:29:38 +0200

    Ustawienia aplikacji - PreferenceFragment i SheredPreferenes    1. Zdefinowanie widoków ekranu <PreferenceScreen > ustawień programu w pliku zasobów: res/xml/preferences.xml    2. Zdefinowanie tekstów wartosciami łancuchow znakow w plikach zasobów:        string.xml        array.xml    3. Utworzenie nowej klasy Settings pochodnej od fragmentu PreferenceFragment    4. Połaczenie widoków ekranu ustawien zasobów XML z klasą obsługująca ustawienia aplikacji.    	  W metodzie cyklu życia onCreate przesłaniam:    		addPreferencesFromResource(R.xml.preferences);    5. Odbieranie wyników ustawień w metodzie cyklu zycia onDestroyView:    6. Zapis ustawień w plikach SheredPreferences

--- A RSS MReader/src/com/micromate/mreader/SettingFragment.java
+++ A RSS MReader/src/com/micromate/mreader/SettingFragment.java
@@ -1,21 +1,61 @@
 package com.micromate.mreader;
 
-import android.app.Fragment;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
-public class SettingFragment extends Fragment {
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+public class SettingFragment extends PreferenceFragment {
     
-   public SettingFragment(){}
-    
-   @Override
-   public View onCreateView(LayoutInflater inflater, ViewGroup container,
-           Bundle savedInstanceState) {
- 
-       View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
+	private static final String LOG_TAG= "SettingFragment"; 
+	
+
+	@Override
+     public void onCreate(final Bundle savedInstanceState) {	 
+         super.onCreate(savedInstanceState);
+ 		 Log.i(LOG_TAG, "onCreate");
          
-       return rootView;
-   }
+         //Inflates the given XML resource
+         addPreferencesFromResource(R.xml.preferences);
+     
+	 }
+	 
+	@Override
+	public void onDestroyView() {
+		// TODO Auto-generated method stub
+		super.onDestroyView();
+		Log.i(LOG_TAG, "onDestroyView");
+		settingsService();
+	}
+	 
+	 
+	//Ustawienia i zarzadzanie us¸ugˆ
+	 private void settingsService() {
+		 //Zapis ustawieÄ w plikach SheredPreference
+		 SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+			
+		 //pobranie rezultatow ustawien urzytkownika
+		 boolean powiadomienia = sharedPreferences.getBoolean("prefNotification", false);
+		 String intervalSecond = sharedPreferences.getString("prefIntervalSecond", "60");
+		
+		 Log.i(LOG_TAG, "Notifications: "+powiadomienia);
+		 Log.i(LOG_TAG, "Notification intervals: " +intervalSecond);
+		 /*	//klasa zarzadza us¸uga
+			sheduleAlarmService = new SheduleAlarmService(this);
+			
+			
+			sheduleAlarmService.intervalSecond(Integer.parseInt(intervalSecond));
+			
+			if (powiadomienia) {
+				sheduleAlarmService.start();
+			}
+			else {
+				sheduleAlarmService.stop();
+			} */
+		}
+	 
+	 
+	 
 }
